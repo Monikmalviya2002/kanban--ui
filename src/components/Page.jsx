75c6760ecdf7dd0e528a114fb52144ec683fc1aa
Monik Malviya <monikmalviya04gmail.com>
2025-11-20 21:58:48 +0530

    fix the issue

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import ProjectAreaHeader from "./ProjectArea";
 import ProjectTasks from "./ProjectTasks";
 
 
@@ -9,7 +8,7 @@ export default function Home() {
     <div className=" bg-gray-00 min-h-screen w-full">
       <Navbar />
 
-      <div className="grid grid-cols-[3fr_1fr] gap-6 px-8 py-6 min-h-screen">
+      <div className="grid grid-cols-[3fr_1fr] gap-7 px-8 py-6 min-h-screen">
 
         
         <div className="space-y-6">
